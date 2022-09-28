b18ec6c63e417568645fb77a35f7443b0d75c907
renata-amorim <renata.silva04@hotmail.com>
2022-09-28 18:02:57 -0300

    correção ortográfica

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
                 <img src={renata} alt="Mulher negra com cabelo alisado"/>           
             </div>
             <div className={styles.subtitulo}>
-                <p>Aos 32 anos, depois de me dedicar a Nutricão por 10 anos entre formação,<br/> pós graduação e atuação como Nutricionista, resolvi buscar novos horizontes.<br/> Sempre me interessei por elementos tecnológicos e ao pesquisar uma nova profissão,<br/> gostei da área de Front-end. Comecei estudar e me apaixonei,<br/> é muito legar ver as coisas ficando prontas *-* </p>
+                <p>Aos 32 anos, depois de me dedicar a Nutricão por 10 anos entre formação,<br/> pós graduação e atuação como Nutricionista, resolvi buscar novos horizontes.<br/> Sempre me interessei por elementos tecnológicos e ao pesquisar uma nova profissão,<br/> gostei da área de Front-end. Comecei estudar e me apaixonei,<br/> é muito legal ver as coisas ficando prontas *-* </p>
             </div>
         </>
         
