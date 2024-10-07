82905620bd548e2011f7ccf98b2477d76fe528b0
Robson Amorim <robson.amorim@outlook.com>
2024-10-07 20:20:17 -0300

    add some features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,6 +14,9 @@ function Home() {
             <div className={styles.subtitulo}>
                 <p>Aos 32 anos, depois de me dedicar a Nutricão por 10 anos entre formação,<br/> pós graduação e atuação como Nutricionista, resolvi buscar novos horizontes.<br/> Sempre me interessei por elementos tecnológicos e ao pesquisar uma nova profissão,<br/> gostei da área de Front-end. Comecei estudar e me apaixonei,<br/> é muito legal ver as coisas ficando prontas *-* </p>
             </div>
+            <div className={styles.subtitulo}>
+                <p>Em 20222 me tornei mãe do Leandro!</p>
+            </div>
         </>
         
     )
